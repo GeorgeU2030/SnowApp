045ac89e4742ca60776711d9bfa6a0733b40d716
GeorgeU2030 <georgeu2021@gmail.com>
2024-04-08 15:28:20 -0500

    feat: get_users_has_been_added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -37,4 +37,17 @@ export const signIn = async (req, res) => {
     res.header("Authorization", token).json({ message: token });
 }
 
+export const getUser = async (req,res) => {
+    const id = req.params.id;
+    try {
+        const user = await User.findById(id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        return res.json(user);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
   
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createAdmin,signUp,signIn } from "../controllers/user.controller.js";
+import { createAdmin,signUp,signIn,getUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.post("/signUp", signUp)
 router.post("/createAdmin", createAdmin)
 router.post("/signIn", signIn)
+router.get("/getUser/:id", getUser)
 
 export default router;
